41cd085df23d32aae9cc38437dbcbf1c33f55deb
Riley Anderson <10803452@uvu.edu>
2024-07-05 15:04:24 -0600

    Updated the tank controlles

---
+++ TankTitan/Assets/TEst.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TEst : MonoBehaviour
+{
+    public float mouseSenstivity = 0.1f;
+
+    private void Start()
+    {
+        
+    }
+}
